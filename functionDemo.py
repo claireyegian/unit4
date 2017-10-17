e8d72d0ec4315a870b9e9f42284b4055365a00d2
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-17 14:40:55 -0400

    Updated from Brython Server: 10/17/2017 2:40:53 PM

--- functionDemo.py
+++ functionDemo.py
@@ -1,1 +1,22 @@
+#Claire Yegian
+#10/17/17
+#functionDemo.py - learning functions
 
+def hw():
+    print('Hello, world!')
+
+def bigger(num1,num2): #prints which number is bigger
+    if num1>num2:
+        print(num1)
+    else:
+        print(num2)
+
+def slope(x1,y1,x2,y2): #calculates slope
+    print((y2-y1)/(x2-x1))
+
+#tests for the various functions
+hw()
+bigger(13,27)
+bigger(-10,-15)
+bigger('Smeds','Yegian')
+slope(1,2,2,4)
