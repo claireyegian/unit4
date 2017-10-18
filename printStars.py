b7c5e7400a238c7a27dd2d0d0e4987e40d136190
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-18 13:51:00 -0400

    Updated from Brython Server: 10/18/2017 1:50:59 PM

--- printStars.py
+++ printStars.py
@@ -1,1 +1,12 @@
+#Claire Yegian
+#10/18/17
+#printStars.py - prints input number of stars in pyramid
 
+def printnStars(num1):
+    i=1
+    while i<=num1:
+        for t in range(num1,0,-1):
+            print(' '*t+'*'*i+'*'*i)
+            i += 1
+
+printnStars(12)
