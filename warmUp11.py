1fa31688094134bbb548f48b2c20a648234aa2ca
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-23 14:23:13 -0400

    Updated from Brython Server: 10/23/2017 2:23:13 PM

--- warmUp11.py
+++ warmUp11.py
@@ -1,1 +1,15 @@
+#Claire Yegian
+#10/23/17
+#warmUp11.y - determines if a number is prime
 
+def isPrime(n):
+    i = 2
+    while i<=n:
+        if n%i==0:
+            break
+        return False
+        elif:
+            i += 1
+        return True
+
+isPrime()
