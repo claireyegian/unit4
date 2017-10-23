adbc41ce13dca4e75bd314c5811cab0054a0f12e
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-23 14:54:42 -0400

    Updated from Brython Server: 10/23/2017 2:54:43 PM

--- printSquares.py
+++ printSquares.py
@@ -1,1 +1,16 @@
-p
+#Claire Yegian
+#10/18/17
+#printSquares.py - prints out grid of squares
+
+print(+--+)
+print(|  |)
+print(+--+)
+
+if i==1 and n==1:
+    print(+--+)
+    print(|  |)
+    print(+--+)
+elif i>1 and n==1:
+    print('+--+'+'--+'*(i-1))
+    print(|  |)
+    print(+--+)
