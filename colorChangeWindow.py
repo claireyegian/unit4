8b5a2301f1c35744a52a9a9c3d7ca6d5e23b225b
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-23 14:40:59 -0400

    Updated from Brython Server: 10/23/2017 2:40:59 PM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -1,3 +1,21 @@
 #Claire Yegian
 #10/18/17
 #colorChangeWindow.py - window changes to random color each time you click it
+
+from random import randint
+
+def mouseClick(event):
+    num1 = randint(0,9)
+    num2 = randint(0,9)
+    num3 = randint(0,9)
+    num4 = randint(0,9)
+    num5 = randint(0,9)
+    num6 = randint(0,9)
+    
+    color = Color(0xnum1num2num3num4num5num6,1)
+    
+    colorBox = RectangleAsset(1000,600,LineStyle(1,color),color)
+    Sprite(colorBox)
+
+App().listenKeyEvent('click',mouseClick)
+App().run()
