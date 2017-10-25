5472c0513e47bde1aecbfa70ff6637ce774dd9b3
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-25 14:00:38 -0400

    Updated from Brython Server: 10/25/2017 2:00:39 PM

--- printSquares.py
+++ printSquares.py
@@ -5,31 +5,8 @@
 def printSquares(rows,columns):
     n = 0
     while n<=columns:
-        if columns == 0:
-            break
-        if columns == 1:
-            print('+--+')
-            print('|  |')
-            print('+--+')
-            n+=1
-        if columns > 1:
-            print('+--+'+'--+'*columns-1)
-            print('|  |'+'  |'*columns-1)
-            print('+--+'+'--+'*columns-1)
-            n+=1
-    i = 0
-    while i<=rows:
-        if rows == 0:
-            break
-        if rows == 1:
-            print('+--+')
-            print('|  |')
-            print('+--+')
-            i+=1
-        if rows > 1:
-            print('+--+'+'--+'*rows-1)
-            print('|  |'+'  |'*rows-1)
-            print('+--+'+'--+'*rows-1)
-            i+=1
+        print('+--'*rows+'+')
+        print('|  '*rows+'|')
+    n += 1
 
 printSquares(2,4)
