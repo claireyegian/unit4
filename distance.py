22b3aca5320e00795255a4e3847742f867a6563a
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-26 14:22:16 -0400

    Updated from Brython Server: 10/26/2017 2:22:13 PM

--- distance.py
+++ distance.py
@@ -1,1 +1,8 @@
-d
+#Claire Yegian
+#10/26/17
+#distance.py - takes two points and finds distance between
+
+def distance(x1,y1,x2,y2):
+    return (((x2-x1)**2+(y2-y1)**2)**(1/2))
+
+print(distance(3,4,-5,2))
