e62f16ef9520f2f4c9e37030530d74a98d915013
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-26 14:39:17 -0400

    Updated from Brython Server: 10/26/2017 2:39:19 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,13 @@
+#Claire Yegian
+#10/26/17
+#stringUnion.py - takes two strings and returns all letters that appear in either word
+
+def stringUnion(word1,word2):
+    string = ''
+    for ch in word1 + word2:
+        if not ch.lower() in string:
+            string = string+ch.lower()
+    return string
+
+print(stringUnion('Mississippi','Pensylvania'))
 
