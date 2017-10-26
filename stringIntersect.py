f3eac672496a31b06b6bd66f7960756410c74d8f
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-26 14:44:01 -0400

    Updated from Brython Server: 10/26/2017 2:44:03 PM

--- stringIntersect.py
+++ stringIntersect.py
@@ -1,1 +1,12 @@
+#Claire Yegian
+#10/26/17
+#stringIntersect.py - returns all letters in both words
 
+def stringIntersect(word1,word2):
+    string = ''
+    for ch in word1 + word2:
+        if (not ch.lower() in string) and (ch.lower() in word1 and word2):
+            string = string + ch.lower()
+    return string
+
+print(stringIntersect('Mississippi','Pensylvania'))
