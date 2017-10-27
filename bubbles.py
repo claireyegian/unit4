a568f4f618c1a0247143457dd15d641ef26ab5dc
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-27 14:54:43 -0400

    Updated from Brython Server: 10/27/2017 2:54:47 PM

--- bubbles.py
+++ bubbles.py
@@ -1,1 +1,22 @@
+#Claire Yegian
+#10/27/17
+#bubbles.py - random circle appears each time user clicks screen
 
+from ggame import *
+from random import randint
+
+num = randint(1,7)
+color1 = Color(0x00ff21,1)
+color2 = Color(0x00ff21,1)
+color1 = Color(0x00ff21,1)
+color1 = Color(0x00ff21,1)
+color1 = Color(0x00ff21,1)
+color1 = Color(0x00ff21,1)
+color1 = Color(0x00ff21,1)
+
+bubblecolor = Color(0x004566,1)
+
+bubble = CircleAsset(randint(1,200),LineStyle(1,bubblecolor),bubblecolor)
+
+Sprite(bubble,(randint(1,500),randint(1,500)))
+App().run()
