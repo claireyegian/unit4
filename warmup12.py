19aadaab7e116f017452981135f5ed99b9518c85
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-27 14:26:23 -0400

    Updated from Brython Server: 10/27/2017 2:26:26 PM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,16 @@
+#Claire Yegian
+#10/27/17
+#warmup12.py - finds GCF of two numbers
 
+def GCF(num1,num2):
+    i == min(num1,num2)
+    while i>=0:
+        if num1%i==0:
+            if num2%i==0:
+                return(i)
+                break
+        elif num1%i!=0:
+            break
+    i = i - 1
+
+print(GCF(10,15))
