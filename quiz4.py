9dd51eb55f6d438036069b4d21b43971d4faaf9c
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-30 14:35:23 -0400

    Update quiz4.py

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,24 @@
+#Claire Yegian
+#10/30/17
+#quiz4.py - does a bunch of random stuff
 
+def csia():
+    print('computer science is awesome'*5)
+
+def average(num1,num2,num3):
+    return((num1+num2+num3)/3)
+
+def lastLetter(string):
+    length = string
+    print(length)
+
+def same(arg1,arg2):
+    if arg1==arg2:
+        return('True')
+    else:
+        return('False')
+
+csia()
+print(average(1,2,3))
+lastLetter('Smedinghoff')
+print(same(2*3,7-1))
