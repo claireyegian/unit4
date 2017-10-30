e1a7991bc27487d218ce6d0bc48948d12376b28e
claireyegian <31485787+claireyegian@users.noreply.github.com>
2017-10-30 14:45:30 -0400

    Update quiz4.py

--- quiz4.py
+++ quiz4.py
@@ -3,14 +3,18 @@
 #quiz4.py - does a bunch of random stuff
 
 def csia():
-    print('computer science is awesome'*5)
+    print('Computer science is awesome')
+    print('Computer science is awesome')
+    print('Computer science is awesome')
+    print('Computer science is awesome')
+    print('Computer science is awesome')
 
 def average(num1,num2,num3):
     return((num1+num2+num3)/3)
 
 def lastLetter(string):
-    length = string
-    print(length)
+    for ch in string:
+        print(ch)
 
 def same(arg1,arg2):
     if arg1==arg2:
@@ -20,5 +24,5 @@ def same(arg1,arg2):
 
 csia()
 print(average(1,2,3))
-lastLetter('Smedinghoff')
+lastLetter('Smedinghoff') #I couldn't get this function to print only the last letter
 print(same(2*3,7-1))
